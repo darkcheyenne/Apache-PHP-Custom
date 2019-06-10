136a4c507374e35ba6792010ef7b858bf80d8ced
Andreas Eberli <andreas@eberli.me>
2019-06-10 11:56:33 +0200

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,6 +1,6 @@
 #/bin/bash
-docker build --pull --no-cache -t darkcheyenne/apache-php-custom:20190508 .
+docker build --pull --no-cache -t darkcheyenne/apache-php-custom:20190510 .
 docker build --pull --no-cache -t darkcheyenne/apache-php-custom:latest .
 
-docker push darkcheyenne/apache-php-custom:20190508
+docker push darkcheyenne/apache-php-custom:20190510
 docker push darkcheyenne/apache-php-custom:latest
