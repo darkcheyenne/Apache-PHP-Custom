54307f7f4d19772019bec0e36529d5b503d3e7f7
Andreas Eberli <andreas@eberli.me>
2019-09-19 11:24:29 +0200

    Hinzufügen von Tag für Github Repository

--- build.sh
+++ build.sh
@@ -1,4 +1,5 @@
 #/bin/bash
-docker build --pull --no-cache -t darkcheyenne/apache-php-custom:latest .
+docker build --pull --no-cache -t darkcheyenne/apache-php-custom:latest -t docker.pkg.github.com/darkcheyenne/apache-php-custom/base:latest .
 
 docker push darkcheyenne/apache-php-custom:latest
+docker push docker.pkg.github.com/darkcheyenne/apache-php-custom/base:latest
