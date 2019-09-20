2452f52463b03c6d4a06eeb955502cce6d747612
Andreas Eberli <andreas@eberli.me>
2019-09-20 11:13:12 +0200

    Hinzufügen von Github als Image Ziel

--- build.sh
+++ build.sh
@@ -1,5 +1,5 @@
 #/bin/bash
-docker build --pull --no-cache -t darkcheyenne/apache-php-custom:latest -t docker.pkg.github.com/darkcheyenne/apache-php-custom/base:latest .
+docker build --pull --no-cache -f Dockerfile -t docker.pkg.github.com/darkcheyenne/apache-php-custom/base:latest -t darkcheyenne/apache-php-custom:latest .
 
 docker push darkcheyenne/apache-php-custom:latest
 docker push docker.pkg.github.com/darkcheyenne/apache-php-custom/base:latest
