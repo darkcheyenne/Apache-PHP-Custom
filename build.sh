c56820d9adb1e87097e8fb31c0f5d99b124029a8
Andreas Eberli <andreas@eberli.me>
2020-05-29 08:55:13 +0000

    Update auf Ubuntu 20.04

--- build.sh
+++ build.sh
@@ -1,9 +1,11 @@
 #/bin/bash
-logger Start von Erstellung von Docker Image Apache PHP Custom
-docker build --pull -f Dockerfile.dockerhub -t darkcheyenne/apache-php-custom:latest . | grep 'built\|tagged' | logger
-docker build --pull -f Dockerfile.github -t docker.pkg.github.com/darkcheyenne/apache-php-custom/apache-php-custom:latest . | grep 'built\|tagged' | logger
+echo 🚀 Start von Erstellung von Docker Image Apache PHP Custom
+echo "  🔧 Baue Image für Docker-Hub"
+docker build --no-cache --pull -f Dockerfile.dockerhub -t darkcheyenne/apache-php-custom:latest .
+echo "  🔧 Baue Image für GitHub"
+docker build --no-cache --pull -f Dockerfile.github -t docker.pkg.github.com/darkcheyenne/apache-php-custom/apache-php-custom:latest .
 
-logger Upload von Apache PHP Custom zu Dockerhub
+echo "  🛰 Upload von Apache PHP Custom zu Dockerhub"
 docker push darkcheyenne/apache-php-custom:latest
-logger Upload von Apache PHP Custom zu Github
+echo "  🛰 Upload von Apache PHP Custom zu Github"
 docker push docker.pkg.github.com/darkcheyenne/apache-php-custom/apache-php-custom:latest
